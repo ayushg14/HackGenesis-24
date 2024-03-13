a4ff92857aa2b31990b8d6062e5f0b22c0db0457
Ayush Gupta <ayushg1408@gmail.com>
2024-03-14 00:52:26 +0530

    Iphone Size Countdown

--- HackGenesis/src/components/CountdownTimer.jsx
+++ HackGenesis/src/components/CountdownTimer.jsx
@@ -56,8 +56,8 @@ const CountdownTimer = () => {
           : `${countdown.days}d ${countdown.hours}h ${countdown.minutes}m ${countdown.seconds}s`}
       </p> */}
       <div className="grid grid-flow-col gap-5 text-center auto-cols-max max-sm:h-[6rem]">
-        <div className="flex flex-col p-2 bg-gray-800 rounded-box text-neutral-content justify-center max-sm:h-[20rem]">
-          <span className="countdown font-mono text-[6rem] max-sm:text-[2.3rem] mb-[0.5rem] text-[yellow]">
+        <div className="flex flex-col p-2 bg-gray-800 rounded-box text-neutral-content justify-center">
+          <span className="countdown font-mono text-[6rem] max-sm:text-[1.3rem] mb-[0.5rem] text-[yellow]">
             <span style={{ "--value": countdown.days }}>{countdown.days}</span>
           </span>
           Days
